5e034946ab6f066024ed8996bbae4199ec882818
Junior8288 <160739211+Junior8288@users.noreply.github.com>
2024-05-17 15:16:02 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/automated_unit_testing_setup/Automated_Unit_Testing_Setup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_unit_testing_setup;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Automated_Unit_Testing_Setup {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/mycompany/automated_unit_testing_setup/Unit_TestingTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.automated_unit_testing_setup;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Unit_TestingTest {
+    
+    public Unit_TestingTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
